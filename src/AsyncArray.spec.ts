3b0fe19210e81753623ba72ac8ffb49bb4621620
sebowykubiak <sebowykubiak@gmail.com>
2021-09-13 17:16:34 +0200

    test(filter): Add filter test

--- src/AsyncArray.spec.ts
+++ src/AsyncArray.spec.ts
@@ -7,12 +7,25 @@ describe('AsyncArray', () => {
       const expected: number[] = [3, 2, 1];
 
       const asyncArray: AsyncArray<number> = new AsyncArray(nativeArray);
-
       await asyncArray.sort(async (left: number, right: number): Promise<number> => {
         return await Promise.resolve(right - left);
       });
 
       expect(nativeArray).toEqual(expected);
+    });
+  });
+
+  describe('filter()', () => {
+    it('should filter array with provided predicate', async () => {
+      const nativeArray: number[] = [1, 3, 2];
+      const expected: number[] = [3, 2];
+
+      const asyncArray: AsyncArray<number> = new AsyncArray(nativeArray);
+      const filteredArray = await asyncArray.filter(async (element: number): Promise<boolean> => {
+        return Promise.resolve(element > 1);
+      });
+
+      expect(filteredArray).toEqual(expected);
     });
   });
 
